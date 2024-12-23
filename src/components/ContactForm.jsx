75525924661af50dcefe8980b8f4c233906f3f46
SMHussein <saifabobahjat@gmail.com>
2024-12-23 21:51:28 +0300

    Adding new domain

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,140 +1,147 @@
-'use client';
+"use client";
 
-import { useForm } from 'react-hook-form';
-import Button from './Button';
-import { toast } from 'react-hot-toast';
-import { useFormState } from 'react-dom';
-import { contactUs } from '@utils/actions';
-import { useEffect, useState } from 'react';
-import emailjs from '@emailjs/browser';
+import { useForm } from "react-hook-form";
+import Button from "./Button";
+import { toast } from "react-hot-toast";
+import { useFormState } from "react-dom";
+import { contactUs } from "@utils/actions";
+import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const initialState = {
-    message: '',
-    error: '',
-    id: '',
+  message: "",
+  error: "",
+  id: "",
 };
 
 function ContactForm({
-    name,
-    email,
-    phone,
-    message,
-    send,
-    emailValidation,
-    phoneValidation,
-    requiredValidation,
-    phoneLengthValidation,
-    successMessage,
+  name,
+  email,
+  phone,
+  message,
+  send,
+  emailValidation,
+  phoneValidation,
+  requiredValidation,
+  phoneLengthValidation,
+  successMessage,
 }) {
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-    const [formState, formAction] = useFormState(contactUs, initialState);
+  const [formState, formAction] = useFormState(contactUs, initialState);
 
-    const onSubmit = async (data) => {
-        const { name, email, phone, message } = data;
+  const onSubmit = async (data) => {
+    const { name, email, phone, message } = data;
 
-        if (!name || !email || !phone || !message) return;
+    if (!name || !email || !phone || !message) return;
 
-        formAction(data);
-        sendEmail(name, email, message, phone);
-        reset();
-    };
+    formAction(data);
+    sendEmail(name, email, message, phone);
+    reset();
+  };
 
-    const sendEmail = async (name, email, message, phone) => {
-        try {
-            const emailParams = {
-                name,
-                phone,
-                email,
-                message,
-            };
+  const sendEmail = async (name, email, message, phone) => {
+    try {
+      const emailParams = {
+        name,
+        phone,
+        email,
+        message,
+      };
 
-            await emailjs.send(
-                process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-                process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-                emailParams,
-                process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-            );
-        } catch (error) {
-            console.error('Failed to send message. Please try again later.', error);
-        }
-    };
+      await emailjs.send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        emailParams,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      );
+    } catch (error) {
+      console.error("Failed to send message. Please try again later.", error);
+    }
+  };
 
-    useEffect(() => {
-        if (formState.message) {
-            toast.success(successMessage);
-        }
-    }, [formState.id]);
+  useEffect(() => {
+    if (formState.message) {
+      toast.success(successMessage);
+    }
+  }, [formState.id]);
 
-    return (
-        <form action={handleSubmit(onSubmit)} className='space-y-4 flex flex-col gap-4'>
-            <div>
-                <label htmlFor='name'>{name}</label>
-                <input
-                    id='name'
-                    {...register('name', { required: true })}
-                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                />
-                {errors.name && <span className='text-red-500 text-sm'>{requiredValidation}</span>}
-            </div>
-            <div>
-                <label htmlFor='phone'>{phone}</label>
-                <input
-                    id='phone'
-                    type='number'
-                    {...register('phone', {
-                        required: phoneValidation,
-                        minLength: {
-                            value: 9,
-                            message: phoneLengthValidation,
-                        },
-                    })}
-                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                />
-                {errors.phone && (
-                    <span className='text-red-500 text-sm'>{errors.phone.message}</span>
-                )}
-            </div>
+  return (
+    <form
+      action={handleSubmit(onSubmit)}
+      className="space-y-4 flex flex-col gap-4"
+    >
+      <div>
+        <label htmlFor="name">{name}</label>
+        <input
+          id="name"
+          {...register("name", { required: true })}
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
+        {errors.name && (
+          <span className="text-red-500 text-sm">{requiredValidation}</span>
+        )}
+      </div>
+      <div>
+        <label htmlFor="phone">{phone}</label>
+        <input
+          id="phone"
+          type="number"
+          {...register("phone", {
+            required: phoneValidation,
+            minLength: {
+              value: 9,
+              message: phoneLengthValidation,
+            },
+          })}
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
+        {errors.phone && (
+          <span className="text-red-500 text-sm">{errors.phone.message}</span>
+        )}
+      </div>
 
-            <div>
-                <label htmlFor='email'>{email}</label>
-                <input
-                    id='email'
-                    {...register('email', {
-                        required: requiredValidation,
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: emailValidation,
-                        },
-                    })}
-                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                />
-                {errors.email && (
-                    <span className='text-red-500 text-sm'>{errors.email.message}</span>
-                )}
-            </div>
-            <div>
-                <label htmlFor='message'>{message}</label>
-                <textarea
-                    rows={10}
-                    id='message'
-                    {...register('message', { required: true })}
-                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
-                ></textarea>
-                {errors.message && (
-                    <span className='text-red-500 text-sm'>{requiredValidation}</span>
-                )}
-            </div>
-            <div>
-                <Button className='flex gap-4 items-center justify-center'>{send}</Button>
-            </div>
-        </form>
-    );
+      <div>
+        <label htmlFor="email">{email}</label>
+        <input
+          id="email"
+          {...register("email", {
+            required: requiredValidation,
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: emailValidation,
+            },
+          })}
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
+        {errors.email && (
+          <span className="text-red-500 text-sm">{errors.email.message}</span>
+        )}
+      </div>
+      <div>
+        <label htmlFor="message">{message}</label>
+        <textarea
+          rows={10}
+          id="message"
+          {...register("message", { required: true })}
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        ></textarea>
+        {errors.message && (
+          <span className="text-red-500 text-sm">{requiredValidation}</span>
+        )}
+      </div>
+      <div>
+        <Button className="flex gap-4 items-center justify-center">
+          {send}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
 export default ContactForm;
--- utils/actions.js
+++ utils/actions.js
@@ -1,218 +1,236 @@
-'use server';
+"use server";
 
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-import { createClient } from '@/utils/supabase/server';
-import { revalidateCache } from './services';
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { revalidateCache } from "./services";
 
 export async function updateContacts(currentState, formData) {
-    revalidateCache();
-    const supabase = createClient();
-    const phoneNumber1 = formData.get('phoneNumber1');
-    const phoneNumber2 = formData.get('phoneNumber2');
-    const email = formData.get('email');
-    const location = formData.get('location');
+  revalidateCache();
+  const supabase = createClient();
+  const phoneNumber1 = formData.get("phoneNumber1");
+  const phoneNumber2 = formData.get("phoneNumber2");
+  const email = formData.get("email");
+  const location = formData.get("location");
 
-    const { error } = await supabase
-        .from('contact')
-        .update({ phoneNumber1, phoneNumber2, email, location })
-        .eq('id', 1);
+  const { error } = await supabase
+    .from("contact")
+    .update({ phoneNumber1, phoneNumber2, email, location })
+    .eq("id", 1);
 
-    if (error) {
-        console.error(error);
-        throw new Error(error.message);
-    }
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
 
-    revalidatePath('/', 'layout');
-    return { message: 'Contacts were updated!' };
+  revalidatePath("/", "layout");
+  return { message: "Contacts were updated!" };
 }
 
 export async function contactUs(currentState, formData) {
-    const supabase = createClient();
-    const { name, phone, email, message } = formData;
+  const supabase = createClient();
+  const { name, phone, email, message } = formData;
 
-    const { data, error } = await supabase
-        .from('messages')
-        .insert([{ name, phone, email, message, status: 'unread' }])
-        .select();
+  const { data, error } = await supabase
+    .from("messages")
+    .insert([{ name, phone, email, message, status: "unread" }])
+    .select();
 
-    if (error) {
-        return { error: 'Something went wrong, please try again later' };
-    }
+  if (error) {
+    return { error: "Something went wrong, please try again later" };
+  }
 
-    const newState = { ...currentState, message: 'Message sent successfully!', id: data[0].id };
+  const newState = {
+    ...currentState,
+    message: "Message sent successfully!",
+    id: data[0].id,
+  };
 
-    return newState;
+  return newState;
 }
 
 export async function createEditClient(currentState, formData) {
-    revalidateCache();
-    const supabase = createClient();
-    const name = formData.get('name');
-    const image = formData.get('image');
-    const editId = formData.get('editId');
+  revalidateCache();
+  const supabase = createClient();
+  const name = formData.get("name");
+  const image = formData.get("image");
+  const editId = formData.get("editId");
 
-    if (!name || !image || (!editId && !image.size)) {
-        return { error: 'Fields should not be empty!' };
-    }
+  if (!name || !image || (!editId && !image.size)) {
+    return { error: "Fields should not be empty!" };
+  }
 
-    const hasImagePath = image?.startsWith?.(process.env.SUPABASE_URL);
+  const hasImagePath = image?.startsWith?.(process.env.SUPABASE_URL);
 
-    const imageName = `${Math.random()}-${image.name}`.replaceAll('/', '');
-    const imagePath = hasImagePath
-        ? image
-        : `${process.env.SUPABASE_URL}/storage/v1/object/public/clients/${imageName}`;
+  const imageName = `${Math.random()}-${image.name}`.replaceAll("/", "");
+  const imagePath = hasImagePath
+    ? image
+    : `${process.env.SUPABASE_URL}/storage/v1/object/public/clients/${imageName}`;
 
-    // 1. Create/edit client
-    let query = supabase.from('clients');
+  // 1. Create/edit client
+  let query = supabase.from("clients");
 
-    // A) CREATE
-    if (!editId) query = query.insert([{ name, image: imagePath }]);
+  // A) CREATE
+  if (!editId) query = query.insert([{ name, image: imagePath }]);
 
-    // B) EDIT
-    if (editId) query = query.update({ name, image: imagePath }).eq('id', editId);
+  // B) EDIT
+  if (editId) query = query.update({ name, image: imagePath }).eq("id", editId);
 
-    const { data, error } = await query.select().single();
+  const { data, error } = await query.select().single();
 
-    if (error) {
-        console.error(error);
-        throw new Error('Client could not be created');
-    }
+  if (error) {
+    console.error(error);
+    throw new Error("Client could not be created");
+  }
 
-    // 2. Upload image
-    if (hasImagePath) {
-        revalidatePath('/');
-        return { success: 'Clients list is updated!' };
-    }
+  // 2. Upload image
+  if (hasImagePath) {
+    revalidatePath("/");
+    return { success: "Clients list is updated!" };
+  }
 
-    const { data: dataImage, error: storageError } = await supabase.storage
-        .from('clients')
-        .upload(imageName, image);
+  const { data: dataImage, error: storageError } = await supabase.storage
+    .from("clients")
+    .upload(imageName, image);
 
-    // 3. Delete the client IF there was an error uplaoding image
-    if (storageError) {
-        await supabase.from('clients').delete().eq('id', data.id);
-        console.error(storageError);
-        throw new Error('client image could not be uploaded and the client was not created');
-    }
+  // 3. Delete the client IF there was an error uplaoding image
+  if (storageError) {
+    await supabase.from("clients").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "client image could not be uploaded and the client was not created"
+    );
+  }
 
-    if (data && dataImage) {
-        revalidatePath('/', 'layout');
-        return { success: 'Clients list is updated!' };
-    }
+  if (data && dataImage) {
+    revalidatePath("/", "layout");
+    return { success: "Clients list is updated!" };
+  }
 }
 
 export async function deleteClient(currentState, client) {
-    revalidateCache();
-    const supabase = createClient();
-    const { id, image } = client;
+  revalidateCache();
+  const supabase = createClient();
+  const { id, image } = client;
 
-    try {
-        const { error: deleteError } = await supabase.from('clients').delete().eq('id', id);
+  try {
+    const { error: deleteError } = await supabase
+      .from("clients")
+      .delete()
+      .eq("id", id);
 
-        if (deleteError) {
-            throw new Error('Client could not be deleted');
-        }
-    } catch (e) {
-        return { error: e.message };
+    if (deleteError) {
+      throw new Error("Client could not be deleted");
     }
+  } catch (e) {
+    return { error: e.message };
+  }
 
-    try {
-        const imagePath = image.split('/').pop(); // Assuming 'image' contains the full URL
+  try {
+    const imagePath = image.split("/").pop(); // Assuming 'image' contains the full URL
 
-        const { error: storageError } = await supabase.storage.from('clients').remove([imagePath]);
+    const { error: storageError } = await supabase.storage
+      .from("clients")
+      .remove([imagePath]);
 
-        if (storageError) {
-            throw new Error('Client deleted, but image could not be removed from storage');
-        }
-    } catch (e) {
-        return { error: e.message };
+    if (storageError) {
+      throw new Error(
+        "Client deleted, but image could not be removed from storage"
+      );
     }
+  } catch (e) {
+    return { error: e.message };
+  }
 
-    revalidatePath('/', 'layout');
-    return { success: 'Client deleted successfully!' };
+  revalidatePath("/", "layout");
+  return { success: "Client deleted successfully!" };
 }
 
 export async function deleteMessage(currentState, id) {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const { error } = await supabase.from('messages').delete().eq('id', id);
+  const { error } = await supabase.from("messages").delete().eq("id", id);
 
-    if (error) {
-        return { error: 'Failed to delete message' };
-    }
+  if (error) {
+    return { error: "Failed to delete message" };
+  }
 
-    revalidatePath('/admin/messages');
+  revalidatePath("/admin/messages");
 
-    return { success: 'Message deleted successfully!' };
+  return { success: "Message deleted successfully!" };
 }
 
 export async function readMessage(currentState, id) {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const { data, error } = await supabase
-        .from('messages')
-        .update({ status: 'read' })
-        .eq('id', id)
-        .select();
+  const { data, error } = await supabase
+    .from("messages")
+    .update({ status: "read" })
+    .eq("id", id)
+    .select();
 
-    if (error) {
-        return { error: 'Failed to update message' };
-    }
+  if (error) {
+    return { error: "Failed to update message" };
+  }
 
-    revalidatePath('/admin/messages');
+  revalidatePath("/admin/messages");
 
-    return { success: 'Message update successfully!' };
+  return { success: "Message update successfully!" };
 }
 
 export async function archiveClient(currentState, client) {
-    revalidateCache();
-    const { id } = client;
-    // Step 2: Insert the client data into the archive table
-    const { error: insertError } = await supabase.from('archive').insert([client]);
+  revalidateCache();
+  const { id } = client;
+  // Step 2: Insert the client data into the archive table
+  const { error: insertError } = await supabase
+    .from("archive")
+    .insert([client]);
 
-    if (insertError) {
-        console.error('Error inserting client into archive:', insertError);
-        return { error: 'Failed to archive client' };
-    }
+  if (insertError) {
+    console.error("Error inserting client into archive:", insertError);
+    return { error: "Failed to archive client" };
+  }
 
-    // Step 3: Delete the client from the clients table
-    const { error: deleteError } = await supabase.from('clients').delete().eq('id', id);
+  // Step 3: Delete the client from the clients table
+  const { error: deleteError } = await supabase
+    .from("clients")
+    .delete()
+    .eq("id", id);
 
-    if (deleteError) {
-        console.error('Error deleting client:', deleteError);
-        return { error: 'Failed to delete client from clients table' };
-    }
+  if (deleteError) {
+    console.error("Error deleting client:", deleteError);
+    return { error: "Failed to delete client from clients table" };
+  }
 
-    revalidatePath('/', 'layout');
-    return { success: 'Client archived successfully' };
+  revalidatePath("/", "layout");
+  return { success: "Client archived successfully" };
 }
 
 export async function login(currentState, formData) {
-    const supabase = createClient();
-    const data = {
-        email: formData.get('email'),
-        password: formData.get('password'),
-    };
+  const supabase = createClient();
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
 
-    const { error } = await supabase.auth.signInWithPassword(data);
+  const { error } = await supabase.auth.signInWithPassword(data);
 
-    if (error) {
-        return { error: error.message };
-    }
+  if (error) {
+    return { error: error.message };
+  }
 
-    redirect('/admin');
+  redirect("/admin");
 }
 
 export async function logout(params) {
-    const supabase = createClient();
-    const { error } = await supabase.auth.signOut();
+  const supabase = createClient();
+  const { error } = await supabase.auth.signOut();
 
-    if (error) {
-        redirect('/error');
-    }
+  if (error) {
+    redirect("/error");
+  }
 
-    redirect('/admin');
+  redirect("/admin");
 }
 
 // info@horizon-sp.com
